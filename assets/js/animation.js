f6b7f2144c91cddab623c69981e1d978cdaa6444
Rashmi Guruge <79540934+RashmiGuruge@users.noreply.github.com>
2026-01-12 09:24:27 +0530

    cursor point & body background

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -8,11 +8,11 @@ $(document).ready(function () {
 });
 
 
-// $(document).ready(function () {
-//   "use strict";
-//   $("body").ripples({
-//     resolution: 512,
-//     dropRadius: 14,
-//     perturbance: 0.0014,
-//   });
-// });
+$(document).ready(function () {
+  "use strict";
+  $("body").ripples({
+    resolution: 512,
+    dropRadius: 14,
+    perturbance: 0.0014,
+  });
+});
